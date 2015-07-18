10f54b589c8b19a5424d1b99b82e42c8bbe2af25
lucasfs7 <lucasfs7@gmail.com>
2015-07-18 20:09:28 +0000

    dont use altContainer inside dumb components - because it will generate a div anyway

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import AltContainer from 'alt/AltContainer'
 
 class Header extends React.Component {
   render() {
     return (
-      <AltContainer>
+      <header>
         <h1>Aldo Fabrini</h1>
         <a href="/about">about</a>
-      </AltContainer>
+      </header>
     )
   }
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import AltContainer from 'alt/AltContainer'
 import Header from '../components/Header'
 import JobsList from '../components/JobsList'
 
 class Home extends React.Component {
   render() {
     return (
-      <AltContainer>
+      <div>
         <Header />
         <JobsList />
-      </AltContainer>
+      </div>
     )
   }
 }
