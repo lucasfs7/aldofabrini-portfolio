db94b79226aeb8e4f0eb6db193ff96a010f93390
lucasfs7 <lucasfs7@gmail.com>
2015-07-20 20:34:23 +0000

    add auth method

--- src/sources/UserSource.jsx
+++ src/sources/UserSource.jsx
@@ -1,7 +1,30 @@
 import UserActions from '../actions/UserActions'
 import Firebase from 'firebase'
+const fbRef = new Firebase('https://aldo-fabrini-portfolio.firebaseio.com')
+
+var authCallback = (err, authData, resolve, reject) => {
+  if (err) return reject(err.message)
+  window.localStorage.setItem('fbAuthToken', authData.token)
+  resolve(authData)
+}
 
 var UserSource = {
+  auth() {
+    return {
+      remote() {
+        return new Promise((resolve, reject) => {
+          let token = window.localStorage.getItem('fbAuthToken')
+          if (!token) return reject(false)
+          fbRef.authWithCustomToken(token, (err, authData) => {
+            authCallback(err, authData, resolve, reject)
+          })
+        })
+      },
+      success: UserActions.updateUser,
+      error: UserActions.userFailed,
+      loading: UserActions.signIn
+    }
+  },
   signIn() {
     return {
       remote(store, credentials) {
@@ -10,10 +33,8 @@ var UserSource = {
             reject('E-mail and password can\'t be blank.')
             return
           }
-          const fbRef = new Firebase('https://aldo-fabrini-portfolio.firebaseio.com')
           fbRef.authWithPassword(credentials, (err, authData) => {
-            if (err) reject(err.message)
-            else resolve(authData)
+            authCallback(err, authData, resolve, reject)
           })
         })
       },
