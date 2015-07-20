54c32d4b9408acfae80285b792dcaedd96f21fe7
lucasfs7 <lucasfs7@gmail.com>
2015-07-20 23:34:39 +0000

    redirect to home if the user is unsined

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 class JobDetails extends React.Component {
   render() {
+    if (this.props.route === 'new') {
+      if (!this.props.user.uid) {
+        window.location.hash = ''
+        return (<div className="hidden"></div>)
+      }
+    }
+  
     return (
       <div>job details page</div>
     )
