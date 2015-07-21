b8a90c866230cfce2d8d0963a7e427f768109044
lucasfs7 <lucasfs7@gmail.com>
2015-07-21 17:08:11 +0000

    render the job from te route

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,28 +1,6 @@
 import React from 'react'
 
 class JobDetails extends React.Component {
-  constructor(props) {
-    super()
-    if (!this.isNew(props)) {
-      this.state = {}
-    } else {
-      this.state = {
-        job: {
-          name: {
-            short: 'Short title',
-            long: 'Job title'
-          },
-          client: {
-            short_name: 'Client',
-            name: 'Client name'
-          },
-          description: 'Job descrption goes here',
-          images: []
-        }
-      }
-    }
-  }
-  
   isNew(nextProps) {
     var props = nextProps || this.props
     if (props.route === 'new') return true
@@ -31,29 +9,65 @@ class JobDetails extends React.Component {
 
   accessAllowed(nextProps) {
     var props = nextProps || this.props
-    if (this.isNew(props) && !props.user.uid) {
+    if (this.isNew(props) && !props.userProps.user.uid) {
       return false
     }
     return true
   }
   
+  verifyAccess(props) {
+    if (!this.accessAllowed(props)) window.location.hash = ''
+  }
+  
+  getJob(props) {
+    var job;
+    if (!this.isNew()) job = props.getJob(props.route)
+    if (!job) return {}
+    return job
+  }
+  
+  componentDidMount() {
+    this.verifyAccess(this.props)
+  }
+  
   componentWillUpdate(nextProps) {
     var props = nextProps || this.props
-    if (!this.accessAllowed(props)) window.location.hash = ''
+    this.verifyAccess(props)
   }
 
   render() {
-    if (!this.state.job) return (<div className="hidden"></div>)
+    var job, schema, name, client, description, images
+    
+    if (this.props.jobsProps.loading) {
+      return (<p>Loading</p>)
+    }
+    
+    if (this.props.jobsProps.errorMessage) {
+      return (<p className="msg error">{this.props.jobsProps.errorMessage}</p>)
+    }
+
+    job = this.getJob(this.props)
+    schema = this.props.jobSchema()
+  
+    if (job.name && job.name.long) name = job.name.long
+    else name = schema.name.long
+    
+    if (job.client && job.client.name) client = job.client.name
+    else client = schema.client.name
+    
+    if (job.description) description = job.description
+    else description = schema.description
+    
+    if (job.images && job.images.length) images = job.images
+    else images = schema.images
   
     return (
       <div className="job-details">
-        <h1 className="job-title">{this.state.job.name.long}</h1>
-        <h2 className="job-client">{this.state.job.client.name}</h2>
-        <div className="job-description">
-          <p>{this.state.job.description}</p>
-        </div>
+        <h1 className="job-title">{name}</h1>
+        <h2 className="job-client">{client}</h2>
+        <div className="job-description">{description}</div>
         <ul className="job-images">
-          {this.state.job.images.map((imgUrl, i) => {
+          {images.map((imgUrl, i) => {
             <li key={i}>
               <img src={imgUrl} alt={`Image ${i}`} />
             </li>
