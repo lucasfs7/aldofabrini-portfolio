98278f80b8f3fcf701d276ce39644f5b08394934
lucasfs7 <lucasfs7@gmail.com>
2015-07-21 01:59:48 +0000

    fetch jobs inside home component and pass the store directly to jobs component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import AltContainer from 'alt/AltContainer'
 import Header from '../components/Header'
-import JobsList from '../components/JobsList'
+import Jobs from '../components/Jobs'
 import Page from '../components/Page'
 import UserStore from '../stores/UserStore'
+import JobStore from '../stores/JobStore'
 
 class Home extends React.Component {
   componentDidMount() {
     UserStore.auth()
+    JobStore.fetchJobs()
   }
 
   render() {
     return (
       <AltContainer store={UserStore}>
         <Header signOut={UserStore.signOut} />
-        <JobsList />
+        <AltContainer store={JobStore}>
+          <Jobs />
+        </AltContainer>
         <Page signIn={UserStore.signIn} />
       </AltContainer>
     )
