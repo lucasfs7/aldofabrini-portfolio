c5391dae002a60674886cff52bd3c4cda5ab5f9c
lucasfs7 <lucasfs7@gmail.com>
2015-07-21 00:51:19 +0000

    improve new and access validation and add initial state for new job

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,16 +1,61 @@
 import React from 'react'
 
 class JobDetails extends React.Component {
-  render() {
-    if (this.props.route === 'new') {
-      if (!this.props.user.uid) {
-        window.location.hash = ''
-        return (<div className="hidden"></div>)
+  constructor(props) {
+    super()
+    if (!this.isNew(props)) {
+      this.state = {}
+    } else {
+      this.state = {
+        name: {
+          short: 'Short title',
+          long: 'Job title'
+        },
+        client: {
+          short_name: 'Client',
+          name: 'Client name'
+        },
+        description: 'Job descrption goes here',
+        images: []
       }
     }
+  }
   
+  isNew(nextProps) {
+    var props = nextProps || this.props
+    if (props.route === 'new') return true
+    else return false
+  }
+
+  accessAllowed(nextProps) {
+    var props = nextProps || this.props
+    if (this.isNew(props) && !props.user.uid) {
+      return false
+    }
+    return true
+  }
+  
+  componentWillUpdate(nextProps) {
+    var props = nextProps || this.props
+    if (!this.accessAllowed(props)) window.location.hash = ''
+  }
+
+  render() {
     return (
-      <div>job details page</div>
+      <div className="job-details">
+        <h1 className="job-title">{this.state.name.long}</h1>
+        <h2 className="job-client">{this.state.client.name}</h2>
+        <div className="job-description">
+          <p>{this.state.description}</p>
+        </div>
+        <ul className="job-images">
+          {this.state.images.map((imgUrl, i) => {
+            <li key={i}>
+              <img src={imgUrl} alt={`Image ${i}`} />
+            </li>
+          })}
+        </ul>
+      </div>
     )
   }
 }
