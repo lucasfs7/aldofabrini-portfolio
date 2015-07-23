7188a60a989ae145d50011cc0f6c16dab56d56c7
lucasfs7 <lucasfs7@gmail.com>
2015-07-23 16:47:54 +0000

    separate job thumb into a new component and render it inside jobs

---
+++ src/components/JobThumb.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class JobThumb extends React.Component {
+  render() {
+    var job, image
+    job = this.props.job
+    
+    if (job.thumb.image) image = <img className="job-thumb-image" src={job.thumb.image} alt={job.name.long} ref="jobThumbImage" />
+    else image = <div className="hidden" />
+    return(
+      <figure className="job-thumb">
+        {image}
+        <figcaption className="job-thumb-description">
+          <span className="job-thumb-name" ref="jobShortName">{job.name.short}</span>
+          <span className="job-thumb-client" ref="jobClientShortName">{job.client.short_name}</span>
+        </figcaption>
+      </figure>
+    )
+  }
+}
+
+export default JobThumb
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
+import JobThumb from '../components/JobThumb'
 
 class Jobs extends React.Component {
   render() {
     var jobsProps = this.props.jobsProps
   
     if (jobsProps.errorMessage) {
-      return (
-        <p>{jobsProps.errorMessage}</p>
-      );
+      return (<p className="msg error">{jobsProps.errorMessage}</p>)
     }
 
     if (jobsProps.loading) {
       return (
-        <p>Loading</p>
+        <p>loading</p>
       )
     }
 
@@ -21,9 +20,11 @@ class Jobs extends React.Component {
         {jobsProps.jobs.map((job, i) => {
           return (
             <li key={i}>
-              <a href={`#${job.slug}`}>{job.name}</a>
+              <a href={`#${job.slug}`}>
+                <JobThumb job={job} />
+              </a>
             </li>
-          );
+          )
         })}
       </ul>
     )
