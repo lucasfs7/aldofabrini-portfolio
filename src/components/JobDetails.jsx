6c93f5f3305d4f5d8772eefba1849df7475d3b58
lucasfs7 <lucasfs7@gmail.com>
2015-07-31 00:41:15 -0300

    add video cover and playback controll method

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 class JobDetails extends React.Component {
+  handleVideoPlayback(e) {
+    e.preventDefault()
+    var t = e.target
+    if (t.paused) {
+      t.play()
+      t.controls = true
+    } else {
+      t.pause()
+      t.controls = false
+    }
+  }
+
   render() {
     var job, children, rmImageBtn
     job = this.props.job
@@ -11,9 +23,21 @@ class JobDetails extends React.Component {
       var isVideo = /\.[(avi|mp4|webm|ogv|ogg|mov)]+$/i
       var isImage = /\.[(jpg|png|gif|svg|bmp)]+$/i
       var file = filename.replace('?dl=1', '')
-      if (isVideo.test(file)) return (<video src={filename} controls={true} />)
-      if (isImage.test(file)) return (<img src={filename} />)
-    }
+      if (isVideo.test(file)) {
+        return (
+          <video 
+            src={filename} 
+            poster="/img/video-poster.png" 
+            onClick={this.handleVideoPlayback.bind(this)} 
+          />
+        )
+      }
+      if (isImage.test(file)) {
+        return (
+          <img src={filename} />
+        )
+      }
+    }.bind(this)
 
     return (
       <div className="job-details">
